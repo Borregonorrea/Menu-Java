546246bf7f155f46cfceac8a50a7a0fb6e8c25df
Borregonorrea <samuel.orregoc@upb.edu.co>
2021-10-26 18:33:59 -0500

    Ingreso de datos validos

--- Java/Programa.java
+++ Java/Programa.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Programa {
     public static void main(String[] args) {
 
@@ -11,10 +12,46 @@ public class Programa {
         System.out.println("5. Salir");
         System.out.println("6. Creditos");
 
+        Scanner in = new Scanner(System.in);
         n_op = ConsoleInput.getInt();
 
         if(n_op == 1){
-          
+         System.out.println("Usted a seleccionado la opción 1");
+         System.out.print("Ingrese su dia de nacimiento: ");
+         Integer d = in.nextInt();
+            if(d > 31){
+              System.out.println("El dato ingresado no es valido");
+              System.exit(0);
+            }
+            if(d < 1){
+              System.out.println("El dato ingresado no es valido");
+              System.exit(0);
+            }
+         System.out.print("Ingrese su mes de nacimiento: ");
+         Integer m = in.nextInt();
+          if(m > 12){
+            System.out.println("El dato ingresado no es valido");
+            System.exit(0);
+          }
+          if(m < 1){
+            System.out.println("El dato ingresado no es valido");
+            System.exit(0);
+          }
+         System.out.print("Ingrese su año de nacimiento: ");
+         Integer a = in.nextInt();
+         if(a > 2021){
+           System.out.println("El dato ingresado no es valido");
+           System.exit(0);
+         }
+         if(a < 1904){
+           System.out.println("El dato ingresado no es valido");
+           System.exit(0);
+         }
+         System.out.println("La fecha que usted ingreso fue: " +  d  +  " / "  +  m  +  " / "  +  a);
+         
+        
+
+
 
         }
         if(n_op == 2){
