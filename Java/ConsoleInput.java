d0c37f29b3c97a5fb6304a635ba3f42bc17b9f30
Juan Manuel vega <juanm.vega@upb.edu.co>
2021-10-25 21:15:15 -0500

    lol

--- Java/ConsoleInput.java
+++ Java/ConsoleInput.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 
-public class ConsoleInput{
+public class ConsoleInput {
 
 	public static Scanner sc = new Scanner(System.in);
 
-	public static int getInt()
-	{
+	public static int getInt() {
 		int temp = 0;
 
-		try{
+		try {
 			// Initialize the Scanner object to use the standard input
-        	temp = sc.nextInt();
-                sc.nextLine();
-	        // Close the Scanner
-		}catch(Exception e)
-		{
+			temp = sc.nextInt();
+			sc.nextLine();
+			// Close the Scanner
+		} catch (Exception e) {
 			refreshScanner();
 			System.out.println("Error de lectura de numero entero");
 			System.out.println(e);
@@ -23,16 +21,14 @@ public class ConsoleInput{
 		return temp;
 	}
 
-	public static float getFloat()
-	{
+	public static float getFloat() {
 		float temp = 0;
 
-		try{
+		try {
 			// Initialize the Scanner object to use the standard input
-	        temp = sc.nextFloat();
-                sc.nextLine();
-        }catch(Exception e)
-		{
+			temp = sc.nextFloat();
+			sc.nextLine();
+		} catch (Exception e) {
 			refreshScanner();
 			System.out.println("Error de lectura de numero flotante");
 			System.out.println(e);
@@ -41,20 +37,19 @@ public class ConsoleInput{
 		return temp;
 	}
 
-	public static String getString(){
+	public static String getString() {
 		String temp = null;
 
-		try{
+		try {
 			// Initialize the Scanner object to use the standard input
-	        // Read strings until the End Of File (EOF)
-	        //System.out.println("Finaliza con Ctrl-D");
-	        //while(sc.hasNext()) {
-	            //String s = sc.next();
-	        //}
-	        temp = sc.nextLine();
-	        // Close the Scanner
-		}catch(Exception e)
-		{
+			// Read strings until the End Of File (EOF)
+			// System.out.println("Finaliza con Ctrl-D");
+			// while(sc.hasNext()) {
+			// String s = sc.next();
+			// }
+			temp = sc.nextLine();
+			// Close the Scanner
+		} catch (Exception e) {
 			refreshScanner();
 			System.out.println("Error de lectura de cadena");
 			System.out.println(e);
@@ -62,20 +57,17 @@ public class ConsoleInput{
 		return temp;
 	}
 
-	public static void refreshScanner()
-	{
-		//sc.close();
+	public static void refreshScanner() {
+		// sc.close();
 		sc = new Scanner(System.in);
 	}
 
-    //System.out.print("Enter a long : ");
-    //long l = sc.nextLong();
+	// System.out.print("Enter a long : ");
+	// long l = sc.nextLong();
 
-    //System.out.print("Enter a boolean : ");
-    //boolean b = sc.nextBoolean();
+	// System.out.print("Enter a boolean : ");
+	// boolean b = sc.nextBoolean();
 
-
-
-    //System.out.print("Enter an double : ");
-    //double d = sc.nextDouble();
+	// System.out.print("Enter an double : ");
+	// double d = sc.nextDouble();
 }
