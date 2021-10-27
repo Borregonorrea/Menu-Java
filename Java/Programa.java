3f2b6b28e2be3727bd48239b45a5328a7f87eb3e
Borregonorrea <samuel.orregoc@upb.edu.co>
2021-10-27 15:48:42 -0500

    Agregando op2
    Co-Authored-By: juanmvegam <91034583+juanmvegam@users.noreply.github.com>

--- Java/Programa.java
+++ Java/Programa.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 public class Programa {
     public static void main(String[] args) {
 
@@ -92,6 +94,40 @@ public class Programa {
 
           }
         if(n_op == 2){
+          try {
+
+            Scanner sca = new Scanner(System.in);
+            Calendar cal = new GregorianCalendar();
+            int mesActual = cal.get(Calendar.MONTH), anyoActual = cal.get(Calendar.YEAR),
+                    diaActual = cal.get(Calendar.DAY_OF_MONTH);
+            int mes, dia, anyo;
+            int mesResultado, diaResultado, anyoResultado;
+            System.out.println("calcular cuantos años tiene");
+            System.out.println("dime el dia");
+            dia = sca.nextInt();
+            System.out.println("dime el mes");
+            mes = sca.nextInt();
+            System.out.println("dime el año");
+            anyo = sca.nextInt();
+            if (dia < 0 | dia > 31) {
+                throw new Exception("el dia esta mal puesto");
+            }
+            if (mes < 0 | mes > 12) {
+                throw new Exception("el dia esta mal puesto");
+            }
+            if (dia < 0) {
+                throw new Exception("el dia esta mal puesto");
+            }
+
+            mesResultado = Math.abs(mes - mesActual);
+            diaResultado = diaActual;
+            anyoResultado = Math.abs(anyo - anyoActual);
+
+            System.out.println(
+                    "Tienes " + anyoResultado + " años " + ", " + mesResultado + " meses " + " y " + diaResultado + " dias");
+        } catch (Exception ex) {
+            System.out.println("ha habido un error" + ex.toString());
+        }
 
         }
         if(n_op == 3){
