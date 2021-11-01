6d334b39c2360a1f5f2b1df0ada79ee0e416987c
juanmvegam <91034583+juanmvegam@users.noreply.github.com>
2021-11-01 12:20:41 -0500

    Add files via upload

--- numerologiacalcu.java
+++ numerologiacalcu.java
@@ -1,24 +1,14 @@
-
-import java.nio.IntBuffer;
-
-import javax.print.attribute.standard.JobHoldUntil;
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class numerologiacalcu {
     public static void main(String[] args) {
-        int tabla[];
-        int mayor=0;
-        int cam;
-
-        cam=Integer.parseInt(JOptionPane.showInputDialog("introduce el total de numeroa"));
-        tabla=new int [cam];
-        for(int 1=0;1<cam;1++)
-        {
-            tabla[1]=Integer.parseInt(JOptionPane.showInputDialog("introduce 1e1 numero:"+(1+1)));
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.print("Introduce un número entero: ");
+        n = sc.nextInt();
+        System.out.println("\nNumeros del 1 al " + n + ": ");
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
         }
-        JOptionPane.showMessageDialog(null,"numero mayor es:"+mayor);
-
-
     }
-
 }
